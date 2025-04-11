b8af7866972c63380f12570fde5516ae9b524242
yash <yashsingh8112004@gmail.com>
2025-04-11 10:57:53 +0530

    Intial commit

---
+++ 1st.py
@@ -0,0 +1,16 @@
+import math
+import random
+import re
+import sys
+import os
+
+
+
+n = int(input("no"))
+if(n%2==0):
+    if n in range(2,5):
+        print(weird)
+    if n in range(6,10):
+        print(not weird)
+    else:
+    print(not weird)
